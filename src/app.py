5238918bbce2b626841ab12cbff9da19eff2f1c9
Iman Rahgozar Abadi <imanrahgozarabadi@Imans-MBP.home>
2024-02-06 23:21:53 -0800

    adding the final files

--- src/app.py
+++ src/app.py
@@ -1,20 +1,42 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
+from flask import Flask, render_template, abort
+import json
+import os
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+def create_app():
+    app = Flask(__name__)
+
+    app_path = os.path.dirname(__file__)
+    with open(app_path+'/projects.json','r') as file:
+        projects = json.load(file)
+
+    slug_to_project = {project["slug"]:project for project in projects}
+
+    @app.route("/")
+    def home():
+        return render_template("home.html", projects=projects)
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
-app.run(debug=True, port = 8569)
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
+
+    @app.route("/project/<string:slug>")
+    def project(slug):
+        if slug not in slug_to_project.keys():
+            abort(404)
+        return render_template(f"project_{slug}.html", project=slug_to_project[slug])
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("404.html"), 404
+
+    return app
+
+#app = create_app()
+#app.run(debug=True, port = 8569)
 
